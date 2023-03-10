de14d880efff433cd638d624a4f9629894f02755
yoooon <dev.yoooon@gmail.com>
2023-03-10 18:52:08 +0900

    add append mixin, function

--- src/pages/main/keyvisual.vue
+++ src/pages/main/keyvisual.vue
@@ -3,6 +3,7 @@ import { reactive } from 'vue';
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
+// import 'swiper/css/pagination';
 
 const props = defineProps({
 });
